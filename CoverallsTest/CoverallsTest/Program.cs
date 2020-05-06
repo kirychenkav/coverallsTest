b88593b5593d70e08f821483347921cfc6975948
Valery Kirichenko <valery.kirichenko@idt.net>
2020-05-06 13:19:50 +0300

    initial commit

---
+++ CoverallsTest/CoverallsTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoverallsTest
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("CoverallsTest");
+        }
+    }
+}
