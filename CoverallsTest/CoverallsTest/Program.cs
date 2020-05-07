ce985e88b217c201c60e88210e54c3230b10e199
Valery Kirichenko <valery.kirichenko@idt.net>
2020-05-07 12:13:59 +0300

    add method

--- CoverallsTest/CoverallsTest/Program.cs
+++ CoverallsTest/CoverallsTest/Program.cs
@@ -7,9 +7,15 @@ namespace CoverallsTest
         public static void Main(string[] args)
         {
             Console.WriteLine(GetCurrentDateString());
+            Console.WriteLine(GetCurrentDateTimeString());
         }
 
         public static string GetCurrentDateString()
+        {
+            return DateTime.Now.ToString("d");
+        }
+        
+        public static string GetCurrentDateTimeString()
         {
             return DateTime.Now.ToString("u");
         }
