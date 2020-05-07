9f11b2f81de09a8355526c08121d5cb98ea3f53c
Valery Kirichenko <valery.kirichenko@idt.net>
2020-05-07 12:10:58 +0300

    add fail tests

--- CoverallsTest/UnitTests/Tests.cs
+++ CoverallsTest/UnitTests/Tests.cs
@@ -26,5 +26,11 @@ namespace UnitTests
             var date = Program.GetCurrentDateString();
             Assert.IsNull(date);
         }
+        
+        [Test]
+        public void FailTest2()
+        {
+            Assert.True(false);
+        }
     }
 }
